b41399773431a7e2c5369fe2d4f552300193ef1f
Fiuse <114389537+Fiw0202@users.noreply.github.com>
2023-10-19 00:37:59 +0700

    remove path

--- src/route/MobileRoute.jsx
+++ src/route/MobileRoute.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Content1 from '../mobilePage/content1';
-import MobileLayout from '../mobilePage/latout';
 
 function MobileRouters() {
     return (
